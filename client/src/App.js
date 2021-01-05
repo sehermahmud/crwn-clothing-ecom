bdd10edee3d3dc2c7fb9ec134508db8880a4ff68
sehermahmud <seher.mahmud@gmail.com>
2021-01-05 15:59:57 +0600

    Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { CircularProgress } from '@material-ui/core';
+
+import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import { GlobalStyle } from './global.styles';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-
-import Header from './components/header/header.component';
-
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const SignInAndSignUpPage = lazy(() =>
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
+);
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -25,16 +28,31 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route
-          exact
-          path="/signin"
-          render={() =>
-            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+        <Suspense
+          fallback={
+            <div
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center',
+              }}
+            >
+              <CircularProgress />{' '}
+              <h4 style={{ textAlign: 'center' }}>Loading...</h4>
+            </div>
           }
-        />
+        >
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
+        </Suspense>
+        <Route exact path="/checkout" component={CheckoutPage} />
       </Switch>
     </div>
   );
